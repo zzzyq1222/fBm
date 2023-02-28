e0187fcbebfa09563cca2b3d724f47f50f579f09
zyq <yz1122@ic.ac.uk>
2023-02-28 14:54:38 +0000

    maruyama method for GBM

--- ApproximateSolution/EulerMethod.py
+++ ApproximateSolution/EulerMethod.py
@@ -14,6 +14,8 @@ class EulerMethod:
         dY_t = theta*(mu-Y_t)dt + sigma*dW_t
         Y_0 = Y_init
     2. GBM
+        dS_t = mu*S_t*dt + sigma*S_t*dW_t
+
     """
 
     def generateDwt(self, n, steps):
@@ -29,16 +31,31 @@ class EulerMethod:
         processes = np.zeros((n, steps))  # store each step
 
         yt = y0
-        for j in range(n):
+        for j in range(n):  # generate n processes
             processes[j][0] = y0
             y = []
             for i in range(1, steps):
                 yt = yt + theta * (mu - yt) * interval + \
                      sigma * math.sqrt(interval) * dWt[j][i]
                 y.append(yt)
-
             processes[j][1:] = y
-            yt = 0
+            yt = y0
+        return processes
+
+    def simulateGBM(self, mu, sigma, s0, timespan=10, interval=0.001, n=10):
+        steps = int(timespan / interval)
+        dWt = self.generateDwt(n, steps)
+        processes = np.zeros((n, steps))  # store each step
+        St = s0
+        for j in range(n):  # generate n processes
+            processes[j][0] = s0
+            s = []
+            for i in range(1, steps):
+                St = St + mu * St * interval + \
+                     sigma * St * math.sqrt(interval) * dWt[j][i]
+                s.append(St)
+            processes[j][1:] = s
+            St = s0
         return processes
 
     def draw_paths(self, paths, timespan, interval, data):
@@ -53,5 +70,9 @@ class EulerMethod:
 if __name__ == '__main__':
     euler_method = EulerMethod()
     results = euler_method.simulateOUProcess(0.7, 1.5, 0.06, 0)
+
+    euler_method.draw_paths(10, 10, 0.001, results)
+
+    results = euler_method.simulateGBM(0.7, 1, 0.5)
 
     euler_method.draw_paths(10, 10, 0.001, results)
