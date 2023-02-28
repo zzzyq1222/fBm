60ea4e3517f0da1462f09292a509f96b46e6b084
zyq <yz1122@ic.ac.uk>
2023-02-28 20:47:17 +0000

    refined Euler method for GBM

--- ApproximateSolution/EulerMethod.py
+++ ApproximateSolution/EulerMethod.py
@@ -6,17 +6,11 @@ import sys
 sys.path.append('..')
 import GaussianNoise.gaussian_noise_simulation as gn
 
+"""
+    Euler-Maruyama method and Refined Euler method
+"""
 
 class EulerMethod:
-    """
-    use Euler-Maruyama method to approximate the solution to
-    1. Ornstein-Uhlenbeck process
-        dY_t = theta*(mu-Y_t)dt + sigma*dW_t
-        Y_0 = Y_init
-    2. GBM
-        dS_t = mu*S_t*dt + sigma*S_t*dW_t
-
-    """
 
     def generateDwt(self, n, steps):
         res = np.zeros([n, steps])
@@ -24,6 +18,16 @@ class EulerMethod:
         for i in range(n):
             res[i, :] = gaussian_noise.generateNGn(steps, 'box-muller')
         return res
+
+    """
+    use Euler-Maruyama method to approximate the solution to
+    1. Ornstein-Uhlenbeck process
+        dY_t = theta*(mu-Y_t)dt + sigma*dW_t
+    2. GBM
+        dS_t = mu*S_t*dt + sigma*S_t*dW_t
+
+    """
+
 
     def simulateOUProcess(self, theta, mu, sigma, y0, timespan=10, interval=0.001, n=10):
         steps = int(timespan / interval)
@@ -58,6 +62,34 @@ class EulerMethod:
             St = s0
         return processes
 
+    """
+    Refined Euler Method:
+        1. Ornstein-Uhlenbeck process
+            dY_t = theta*(mu-Y_t)dt + sigma*dW_t
+            Y_0 = Y_init
+            b' = 0
+        2. GBM
+            dS_t = mu*S_t*dt + sigma*S_t*dW_t
+            b' = sigma
+    """
+    def simulateGBMRefined(self, mu, sigma, s0, timespan=10, interval=0.001, n=10):
+        steps = int(timespan / interval)
+        dWt = self.generateDwt(n, steps)
+        processes = np.zeros((n, steps))  # store each step
+        St = s0
+        for j in range(n):  # generate n processes
+            processes[j][0] = s0
+            s = []
+            for i in range(1, steps):
+                St = St + mu * St * interval + \
+                     sigma * St * math.sqrt(interval) * dWt[j][i] + \
+                     0.5 * sigma * interval*(dWt[j][i]**2 - 1)
+                s.append(St)
+            processes[j][1:] = s
+            St = s0
+        return processes
+
+
     def draw_paths(self, paths, timespan, interval, data):
         fig, ax = plt.subplots()
         x = np.linspace(0, timespan, int(timespan / interval))
@@ -69,10 +101,15 @@ class EulerMethod:
 
 if __name__ == '__main__':
     euler_method = EulerMethod()
-    results = euler_method.simulateOUProcess(0.7, 1.5, 0.06, 0)
 
+    #
+    results = euler_method.simulateOUProcess(0.7, 1.5, 0.06, 0, timespan=7, interval=0.001, n=10)
+    # results = euler_method.simulateOUProcess(2, 0, 1, 1)
+    euler_method.draw_paths(10, 7, 0.001, results)
+
+    results = euler_method.simulateGBM(0, 1, 0.1)
     euler_method.draw_paths(10, 10, 0.001, results)
 
-    results = euler_method.simulateGBM(0.7, 1, 0.5)
-
+    results = euler_method.simulateGBMRefined(0, 1, 0.1)
     euler_method.draw_paths(10, 10, 0.001, results)
+
