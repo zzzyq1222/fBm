0b691fb7f2a7bd9c31249ddf11f0e780532d43cc
zyq <yz1122@ic.ac.uk>
2023-03-08 16:29:12 +0000

    fix bug on rBergomi model

--- FractionalBrownianMotion/rough_Bergomi_model.py
+++ FractionalBrownianMotion/rough_Bergomi_model.py
@@ -10,6 +10,7 @@ import sys
 sys.path.append('..')
 import utils
 import hosking_method as hm
+
 """
 generate Volterra process X
 """
@@ -19,7 +20,7 @@ def volterra_process(timespan, interval):
     alpha = -0.43
     eta = 1.9
     size = int(timespan / interval)
-    #todo H = alpha+0.5
+    # todo H = alpha+0.5
     H = 0.5 + alpha
 
     fBm = hm.hoskingMethodFBm()
@@ -29,20 +30,19 @@ def volterra_process(timespan, interval):
     # simulate W_t_1
     Wt_1 = [((interval * 0.5) ** alpha) * Wt[0]]
     for i in range(1, size):
-        newW = ((interval * 0.5) ** alpha) * (Wt[i] - Wt[i-1])
+        newW = ((interval * 0.5) ** alpha) * (Wt[i] - Wt[i - 1])
         Wt_1.append(newW)
 
     # simulate W_j
-    # 反正是N-1个，所以直接从1开始
     W_j = [Wt[0]]
     for i in range(1, size):
         W_j.append(Wt[i] - Wt[i - 1])
 
     # calculate X
-    # todo: eta
     sigma = math.sqrt(2 * alpha + 1)
     b_star = lambda k: ((k ** (alpha + 1) - (k - 1) ** (alpha + 1)) / (alpha + 1)) ** (1 / alpha)
     g = lambda b, H: (b * interval) ** (H - 0.5)
+
     # column of sigma
     sigma_col = np.ones(size) * sigma
     W = np.zeros((size, size))
@@ -51,40 +51,39 @@ def volterra_process(timespan, interval):
     W_j = np.asarray(W_j)
     for i in range(1, size):
         # todo H
-        W_j = np.delete(W_j,0)
-        W[i, :(size-i)] = W_j * g(b_star(i + 1), H)
+        W_j = np.delete(W_j, 0)
+        W[i, :(size - i)] = W_j * g(b_star(i + 1), H)
 
     # rotate anti-clockwise
     W = np.rot90(W, 1)
     X = np.dot(W, sigma_col)
-    # todo: draw
-    utils.draw_n_paths(1, timespan, interval, [X])
 
     return X, Wt
 
+
 """
+simulation of the stock price in the rBergomi model
 input: Volterra process X
 """
-def simulate_rBergomi_model(X, Wt,timespan, interval):
-    N = int(timespan/interval)
+
+
+def simulate_rBergomi_model(X, Wt, timespan, interval):
+    N = int(timespan / interval)
     Xi_0 = 0.026
     eta = 1.9
     alpha = -0.43
 
     V = [Xi_0]
-    for i in range(1,N):
-        exponent = eta * X[i] - (eta**2) * 0.5 * (i*interval)**(2*alpha+1)
+    for i in range(1, N):
+        exponent = eta * X[i] - (eta ** 2) * 0.5 * (i * interval) ** (2 * alpha + 1)
         Vt = Xi_0 * (math.e ** exponent)
         V.append(Vt)
-    # todo: draw
-    utils.draw_n_paths(1, timespan, interval, [V])
 
     logS = [0]
-    for i in range(1,N):
-        newS = logS[i-1] + math.sqrt(V[i]) * (Wt[i] - Wt[i-1]) - 0.5 * V[i] * interval
+    for i in range(1, N):
+        newS = logS[i - 1] + math.sqrt(V[i]) * (Wt[i] - Wt[i - 1]) - 0.5 * V[i] * interval
         logS.append(newS)
-    # todo: draw
-    utils.draw_n_paths(1, timespan, interval, [logS])
+
     S = [math.e ** s for s in logS]
 
     return logS, S
@@ -93,7 +92,9 @@ def simulate_rBergomi_model(X, Wt,timespan, interval):
 if __name__ == '__main__':
     timespan = 10
     interval = 0.01
-    X, Wt = volterra_process(timespan, interval)
-    logS, S = simulate_rBergomi_model(X, Wt, timespan, interval)
-    utils.draw_n_paths(1, timespan, interval, [S])
-
+    res = []
+    for i in range(3):
+        X, Wt = volterra_process(timespan, interval)
+        logS, S = simulate_rBergomi_model(X, Wt, timespan, interval)
+        res.append(S)
+    utils.draw_n_paths(3, timespan, interval, res)
